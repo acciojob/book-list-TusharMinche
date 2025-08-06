049d52a567ef0b7c53d9bed25ece7701c92a289a
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-06 11:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+function fun1(event){
+        event.preventDefault();
+
+        let title = document.querySelector("#title").value;
+        let author = document.querySelector("#author").value;
+        let isbn = document.querySelector("#isbn").value;
+
+        let tr = document.createElement("tr");
+
+        let td_title = document.createElement("td");
+        td_title.innerHTML = title;
+        let td_author = document.createElement("td");
+        td_author.innerHTML = author;
+        let td_isbn = document.createElement("td");
+        td_isbn.innerHTML = isbn;
+
+        let td_delete = document.createElement("td");
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.className = "delete";
+        deleteBtn.onclick = function() {
+            tr.remove();
+        };
+        td_delete.appendChild(deleteBtn);
+
+        tr.appendChild(td_title);
+        tr.appendChild(td_author);
+        tr.appendChild(td_isbn);
+        tr.appendChild(td_delete);
+
+        document.getElementById("book-list").appendChild(tr);
+
+        document.querySelector("form").reset();
+    }
